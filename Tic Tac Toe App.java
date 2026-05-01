0309548bba86916bff8ae3e3e1422933bfca5f55
dinakar-ss <ds7090@srmist.edu.in>
2026-05-01 12:14:33 +0530

    UC8

--- Tic Tac Toe App.java
+++ Tic Tac Toe App.java
@@ -1,54 +1,18 @@
-import java.util.Random;
-
 /**
  * TicTacToe
- * UC7 allows the computer to make a random valid move
- * by reusing slot conversion and validation logic.
+ * UC8 controls the continuous game loop and alternates
+ * turns until the game ends.
  */
 public class TicTacToe {
 
-    static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
-    };
-
-    static char computerSymbol = 'O';
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     /**
-     * Entry point of the program. Triggers the computer move.
+     * Entry point of the program. Demonstrates the structure
+     * of a continuous game loop.
      */
     public static void main(String[] args) {
-        computerMove();
-        printBoard();
-    }
 
-    /**
-     * Generates random slot values until a valid move is found,
-     * then places the computer symbol on the board.
-     */
-    static void computerMove() {
-        Random rand = new Random();
-        int row, col;
-
-        while (true) {
-            row = rand.nextInt(3); // 0 to 2
-            col = rand.nextInt(3); // 0 to 2
-
-            if (board[row][col] == '-') {
-                board[row][col] = computerSymbol;
-                break;
-            }
-        }
-    }
-
-    /**
-     * Prints the current board.
-     */
-    static void printBoard() {
-        System.out.println("Board:");
-        for (int i = 0; i < 3; i++) {
-            System.out.println(board[i][0] + " " + board[i][1] + " " + board[i][2]);
-        }
     }
 }
